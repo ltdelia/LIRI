04cfcade26de029cb24dd9fc42dd1722837cca58
ltdelia <delia.louis@gmail.com>
2016-04-04 20:15:34 -0400

    LIRI printing all request output to log.txt

--- liri.js
+++ liri.js
@@ -1,5 +1,7 @@
 var keys = require('./keys.js');
 
+var fs = require ('fs');
+
 var command = process.argv[2];
 
 var query = process.argv[3];
@@ -23,11 +25,27 @@ if(command == "my-tweets"){
 	  		if(tweets[i]===undefined){
 	  			console.log("");
 	  		}else{
-			  	console.log(tweets[i].user.name);
-			    console.log(tweets[i].created_at);
-			    console.log(tweets[i].text);
+	  			// declared variables for the tweet content
+	  			var username = tweets[i].user.name;
+	  			var created = tweets[i].created_at;
+	  			var tweet = tweets[i].text;
+
+	  			// logging the tweet to the terminal
+			  	console.log(username);
+			    console.log(created);
+			    console.log(tweet);
+
+				// append the tweet to log.txt
+			    fs.appendFile("log.txt", username, function(err, data){})
+			    fs.appendFile("log.txt", "-----", function(err,data){})
+			    fs.appendFile("log.txt", created, function(err,data){})
+			    fs.appendFile("log.txt", "-----", function(err,data){})
+			    fs.appendFile("log.txt", tweet, function(err,data){})	
+			    fs.appendFile("log.txt", "-----", function(err,data){})
+
 			}
 		}
+		fs.appendFile("log.txt", "*** end of tweets ***", function(err, data){})
 	  }
 	});
 }else if(command == "spotify-this-song"){
@@ -44,16 +62,35 @@ if(command == "my-tweets"){
 		        console.log('Error occurred: ' + err);
 		        return;
 		    }else{
-		    	// console.log(data.tracks.items[0]);
-		    // display the following info 
+		    	// declared variables for spotify content
+		    	var artist = data.tracks.items[0].artists[0].name;
+		    	var song = data.tracks.items[0].name;
+		    	var preview = data.tracks.items[0].preview_url;
+		    	var album = data.tracks.items[0].album.name;
+		    	
+		    	// display the following info to the terminal
+		    	
 		    	//artist(s)
-		    	console.log("Artist: " + data.tracks.items[0].artists[0].name);
+		    	console.log("Artist: " + artist);
 		    	//song name
-		    	console.log("Song: "+ data.tracks.items[0].name);
+		    	console.log("Song: "+ song);
 		    	//preview link of the song from spotify
-		    	console.log("Preview Link: " + data.tracks.items[0].preview_url);
+		    	console.log("Preview Link: " + preview);
 		    	//album the song is a part of 
-		    	console.log("Album: " + data.tracks.items[0].album.name);
+		    	console.log("Album: " + album);
+
+		    	// append the info to log.txt
+
+			    fs.appendFile("log.txt", artist, function(err, data){})
+			    fs.appendFile("log.txt", "-----", function(err,data){})
+			    fs.appendFile("log.txt", song, function(err,data){})
+			    fs.appendFile("log.txt", "-----", function(err,data){})
+			    fs.appendFile("log.txt", preview, function(err,data){})	
+			    fs.appendFile("log.txt", "-----", function(err,data){})
+			    fs.appendFile("log.txt", album, function(err,data){})	
+			    fs.appendFile("log.txt", "-----", function(err,data){})
+
+			 	fs.appendFile("log.txt", "*** end of spotify ***", function(err, data){})   		    	
 		    }
 		});
 	}
@@ -67,24 +104,53 @@ if(command == "my-tweets"){
 	request('http://www.omdbapi.com/?t='+query+'&y=&plot=short&r=json&tomatoes=true', function (error, response, body) {
   		if (!error && response.statusCode == 200) {
   			var json = JSON.parse(body);
-    		// console.log(json) // The entire JSON object 
+
+  			var title = json.Title;
+  			var year = json.Year;
+  			var imdbRating = json.imdbRating;
+  			var country = json.Country;
+  			var language = json.Language;
+  			var plot = json.Plot;
+  			var actors = json.Actors;
+  			var tomatoes = json.tomatoMeter;
+
     	// display the following info
     		//Title
-    		console.log("Title: " + json.Title);
+    		console.log("Title: " + title);
     		//Year
-    		console.log("Year: " + json.Year);
+    		console.log("Year: " + year);
     		//IMDB Rating
-    		console.log("IMDB Rating: " + json.imdbRating);
+    		console.log("IMDB Rating: " + imdbRating);
     		//Country
-    		console.log("Country: "+ json.Country);
+    		console.log("Country: "+ country);
     		//Language
-    		console.log("Language: " + json.Language);
+    		console.log("Language: " + language);
     		//Plot
-    		console.log("Plot: " + json.Plot);
+    		console.log("Plot: " + plot);
     		//Actors
-    		console.log("Actors: " + json.Actors);
+    		console.log("Actors: " + actors);
     		//Rotten Tomatoes Rating
-    		console.log("Rotten Tomatoes Rating: " + json.tomatoMeter + "%");
+    		console.log("Rotten Tomatoes Rating: " + tomatoes + "%");
+
+    	// append to log.txt
+		    fs.appendFile("log.txt", title, function(err, data){})
+		    fs.appendFile("log.txt", "-----", function(err,data){})
+		    fs.appendFile("log.txt", year, function(err, data){})
+		    fs.appendFile("log.txt", "-----", function(err,data){})
+		    fs.appendFile("log.txt", imdbRating, function(err, data){})
+		    fs.appendFile("log.txt", "-----", function(err,data){})
+		    fs.appendFile("log.txt", country, function(err, data){})
+		    fs.appendFile("log.txt", "-----", function(err,data){})
+		    fs.appendFile("log.txt", language, function(err, data){})
+		    fs.appendFile("log.txt", "-----", function(err,data){})
+		    fs.appendFile("log.txt", plot, function(err, data){})
+		    fs.appendFile("log.txt", "-----", function(err,data){})
+		    fs.appendFile("log.txt", actors, function(err, data){})
+		    fs.appendFile("log.txt", "-----", function(err,data){})
+		    fs.appendFile("log.txt", tomatoes, function(err, data){})
+		    fs.appendFile("log.txt", "-----", function(err,data){})
+
+			fs.appendFile("log.txt", "*** end of omdb ***", function(err, data){})		    		    		    		    		    		    		    
   		}
 	})
 }else if(command == "do-what-it-says"){
